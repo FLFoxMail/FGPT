6e6d98bceddac32110ca0c766dab7f0cc67a998c
CRon <1390491039@qq.com>
2025-03-19 22:53:08 +0800

    Update GPT notebook for batch attention

--- model/model.py
+++ model/model.py
@@ -65,23 +65,42 @@ class FGPT(nn.Module):
         return x
 
 
-# 判断 cuda 是否可用
-device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-print(device)
-
 d_model = 768
-d_k = 64
-d_v = 64
-batch_size = 32
+d_k = 768
+d_v = 768
+batch_size = 2
 num_heads = 8
-seq_len = 256
+seq_len = 1024
 d_diff = 128
-n_layer = 768
-x = torch.rand(batch_size, seq_len, d_model)
-model = FGPT(d_k, d_v, d_model, num_heads, d_diff, n_layer)
-# 打印模型参数数量大小，假设是4位量化
-print(sum(p.numel() for p in model.parameters()) * 4 / 1024 / 1024)
-y = model(x)
+n_layer = 32
+epoch = 20
+lr = 0.05
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
-print(y.shape)       
+
+x = torch.rand(batch_size, seq_len, d_model).to(device)
+y = torch.rand(batch_size, seq_len, d_model).to(device)
+memory_before = torch.cuda.memory_allocated()
+model = FGPT(d_k, d_v, d_model, num_heads, d_diff, n_layer)
+model = model.to(device)
+
+
+total_params = sum(p.numel() for p in model.parameters())
+print(f"总参数数量：{total_params/1e9:.2f} B")
+
+criterion = nn.MSELoss()
+optimaizer = torch.optim.Adam(model.parameters(), lr)
+
+
+for i in range(epoch):
+    # 清空梯度
+    optimaizer.zero_grad()
+    y_pred = model(x)
+    loss = criterion(y_pred, y)
+    loss.backward()
+    optimaizer.step()
+    if i % 2 == 0:
+        print(f"Epoch: {i + 1}, Loss: {loss.item()}")
+        
+
         
