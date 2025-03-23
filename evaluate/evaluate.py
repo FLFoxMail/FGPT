8a182cf839b614f0a8d76e3ab38adaf9e11ecfb1
CRon <1390491039@qq.com>
2025-03-23 15:20:25 +0800

    Add comment to evaluate.py

--- evaluate/evaluate.py
+++ evaluate/evaluate.py
@@ -91,7 +91,7 @@
 import torch
 import torchvision.models as models
 from torch.profiler import profile, record_function, ProfilerActivity
-
+# 
 model = models.resnet18().cuda()
 inputs = torch.randn(5, 3, 224, 224).cuda()
 
